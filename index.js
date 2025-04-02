93fa08c1c1acbc28de62170181c6724706e2b8c4
Ant_Stern <fleaowns@gmail.com>
2025-04-02 12:43:18 -0400

    Modified index.js to create .html files and updated index

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const path = require('path');
 const lunr = require('lunr');
 
 const transcriptsDir = './transcripts/';
+const htmlTranscriptsDir = './html_transcripts/';
 const indexFile = './search_index.json';
+
+// Create html_transcripts directory if it doesn't exist
+if (!fs.existsSync(htmlTranscriptsDir)) {
+  fs.mkdirSync(htmlTranscriptsDir);
+}
 
 const documents = [];
 
@@ -13,13 +19,18 @@ function processDirectory(directory) {
     const stats = fs.statSync(filePath);
 
     if (stats.isDirectory()) {
-      // Recursively process subdirectories
       processDirectory(filePath);
     } else if (file.endsWith('.txt')) {
-      // Process .txt files
       const content = fs.readFileSync(filePath, 'utf8');
+      const htmlContent = `<pre>${content}</pre>`; // Convert to HTML <pre> tag
+
+      const htmlFileName = file.replace('.txt', '.html');
+      const htmlFilePath = path.join(htmlTranscriptsDir, htmlFileName);
+
+      fs.writeFileSync(htmlFilePath, htmlContent);
+
       documents.push({
-        id: path.relative(transcriptsDir, filePath), // Use relative path as ID
+        id: htmlFileName, // Use .html file name as ID
         text: content
       });
     }
